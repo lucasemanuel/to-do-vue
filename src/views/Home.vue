7587f0fc4fcaeade732cc8d4f2020cf555ab0821
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-10-20 16:03:19 -0300

    :sparkles: Import task list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,55 +5,20 @@
       sub_title="Não se esqueça das suas atividades"
     ></Header>
     <main class="container is-max-widescreen">
-      <div class="box">
-        <article class="media">
-          <div class="media-content">
-            <div class="content">
-              <form v-on:submit.prevent="onSubmit">
-                <div class="field">
-                  <label for="task" class="label">Adicione sua tarefa</label>
-                  <div class="field has-addons">
-                    <div class="control is-expanded">
-                      <input
-                        class="input"
-                        type="text"
-                        name="task"
-                        id="task"
-                        placeholder="ex: Tirar o lixo"
-                        v-model="task"
-                      />
-                    </div>
-                    <div class="control">
-                      <input type="submit" class="button is-primary" value="Salvar"/>
-                    </div>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-        </article>
-      </div>
+      <Tasks />
     </main>
   </div>
 </template>
 
 <script>
 import Header from '../components/Header.vue';
+import Tasks from '../components/Tasks.vue';
 
 export default {
   name: 'Home',
   components: {
     Header,
-  },
-  data() {
-    return {
-      task: '',
-    };
-  },
-  methods: {
-    onSubmit() {
-      console.log(this.task);
-    },
+    Tasks,
   },
 };
 </script>
