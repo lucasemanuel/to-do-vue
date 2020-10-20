395d0ceb05f42ddd0193dcbf983e61fbb03a5dac
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-10-20 13:30:53 -0300

    :sparkles: Get task on submit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,71 @@
 <template>
   <div id="app">
-    <Header title="Minha Tarefas" sub_title="Não se esqueça das suas atividades"></Header>
-    <h1>Minhas Tarefas</h1>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header
+      title="Minha Tarefas"
+      sub_title="Não se esqueça das suas atividades"
+    ></Header>
+    <main class="container is-max-widescreen">
+      <div class="box">
+        <article class="media">
+          <div class="media-content">
+            <div class="content">
+              <form v-on:submit.prevent="onSubmit">
+                <div class="field">
+                  <label for="task" class="label">Adicione sua tarefa</label>
+                  <div class="field has-addons">
+                    <div class="control is-expanded">
+                      <input
+                        class="input"
+                        type="text"
+                        name="task"
+                        id="task"
+                        placeholder="ex: Tirar o lixo"
+                        v-model="task"
+                      />
+                    </div>
+                    <div class="control">
+                      <input type="submit" class="button is-primary" value="Salvar"/>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </article>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 import Header from '../components/Header.vue';
-import HelloWorld from '../components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
     Header,
-    HelloWorld,
+  },
+  data() {
+    return {
+      task: '',
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.task);
+    },
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Sora', sans-serif;
+  font-family: "Sora", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+main.container {
+  padding: 3rem 1.5rem;
 }
 </style>
