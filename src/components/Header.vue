0b8f73eacd17c51f7a04ec39fe087eefd693693f
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-10-20 11:28:45 -0300

    :sparkles: Add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="hero is-primary is-bold">
+    <div class="hero-body">
+      <div class="container is-max-widescreen">
+        <h1 class="title">{{ title }}</h1>
+        <h2 class="subtitle">{{ sub_title }}</h2>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+    sub_title: String,
+  },
+};
+</script>
+
+<style>
+</style>
